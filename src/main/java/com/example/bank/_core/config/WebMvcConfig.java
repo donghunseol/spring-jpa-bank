be6f530a7083dd4b63e61b480c497dd01feb14d4
seoldonghun <ehdgns5647@gmail.com>
2024-04-08 19:39:12 +0900

    bank Entity setting

--- src/main/java/com/example/bank/_core/config/WebMvcConfig.java
+++ src/main/java/com/example/bank/_core/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.example.bank._core.config;
 
+import com.example.bank._core.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
